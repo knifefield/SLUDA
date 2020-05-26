939976debeb4020fb3af584d3b9fc8d59968ad39
thinkPad <houhaopeng@outlook.com>
2020-05-26 08:20:09 +0800

    add train_AP.sh

---
+++ scripts/train_AP.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+SOURCE=$1
+TARGET=$2
+ARCH=$3
+
+if [ $# -ne 3 ]
+  then
+    echo "Arguments error: <SOURCE> <TARGET> <ARCH>"
+    exit 1
+fi
+
+CUDA_VISIBLE_DEVICES=0 \
+python examples/train_AP.py -ds ${SOURCE} -dt ${TARGET} -a ${ARCH} \
+	--num-instances 4 --lr 0.00035 --iters 400 -b 32 -j 0 --epochs 40 \
+	--soft-ce-weight 0.5 --soft-tri-weight 1.0 --dropout 0.5 --lambda-value 0 \
+	--init-1 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-1/model_best.pth.tar \
+	--init-2 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-2/model_best.pth.tar \
+	--logs-dir logs/${SOURCE}TO${TARGET}/${ARCH}-MMT-AP 
