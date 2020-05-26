97336cde0d09e2ab13e094ba836cff0e22e7b7d2
thinkPad <houhaopeng@outlook.com>
2020-05-26 08:06:25 +0800

    aaa

--- examples/mmt/trainers.py
+++ examples/mmt/trainers.py
@@ -228,8 +228,8 @@ class MMTTrainer(object):
             else:
                 loss_oim_1, p_out_t1 = self.criterion_oim(f_out_t1, targets)
                 loss_oim_2, p_out_t2 = self.criterion_oim(f_out_t2, targets)
-                loss_oim_soft = (self.criterion_oim_soft(f_out_t1, p_out_t2_ema) +
-                                 self.criterion_oim_soft(f_out_t2, p_out_t1_ema))
+                loss_oim_soft = (self.criterion_ce_soft(p_out_t1, p_out_t2_ema) +
+                                 self.criterion_ce_soft(p_out_t2, p_out_t1_ema))
                 print('------------------------use oim loss------------------------------')
                 loss = ((loss_oim_1 + loss_oim_2) * (1 - ce_soft_weight) +
                         (loss_tri_1 + loss_tri_2) * (1 - tri_soft_weight) +
